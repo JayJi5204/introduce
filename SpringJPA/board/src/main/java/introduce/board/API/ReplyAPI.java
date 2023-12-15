493a279c7474b91095b5302261ed29ddf8ded320
jayji5204 <jayji5204@naver.com>
2023-12-16 02:52:26 +0900

    Test

--- SpringJPA/board/src/main/java/introduce/board/API/ReplyAPI.java
+++ SpringJPA/board/src/main/java/introduce/board/API/ReplyAPI.java
@@ -1,6 +1,8 @@
 package introduce.board.API;
 
 import introduce.board.Service.ReplyService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +14,14 @@ public class ReplyAPI {
     private final ReplyService replyService;
 
 
+    @PostMapping("/api/board/{id}/reply")
+    public test test(){
+        return null;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class test{
+
+    }
 }
